1a5b40d0061797e1c04f651d258d7764b8a4898d
rizki <rizki@nadyne.net>
2022-05-23 20:36:07 +0700

    Update

--- resources/views/pages/admin/gallery/edit.blade.php
+++ resources/views/pages/admin/gallery/edit.blade.php
@@ -2,63 +2,58 @@
 
 @section('content')
 
-<div class="container-fluid">
+    <div class="container-fluid">
 
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Edit Gallery</h1>
-    </div>
-
-    @if ($errors->any())
-
-        <div class="alert alert-danger">
-            <ul>
-                
-                @foreach ($errors as $record)
-                    
-                    <li>{{ $error }}</li>
-
-                @endforeach
-                
-            </ul>
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Edit Gallery</h1>
         </div>
-        
-    @endif
 
-    <div class="card shadow">
-        <div class="card-body">
-            <form action="{{ route('gallery.update', $item->id) }}" method="POST" enctype="multipart/form-data">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
 
-                @method('PUT')
-                @csrf
-                
-                <div class="form-group">
-                    <label for="title">Gallery</label>
-                    <select name="travel_package_id" required class="form-control">
-                        <option value="{{ $item->travel_packages_id }}">Jangan Diubah</option>
- 
-                        @foreach ($travel_packages as $travel_package)
-                            <option value="{{ $travel_package->id }}">
-                               {{ $travel_package->title }} 
-                            </option>
-                        @endforeach
- 
-                    </select>
-                </div>
- 
-                <div class="form-group">
-                    <label for="image">IMAGE</label>
-                    <input type="file" class="form-control" name="image" placeholder="Image">
-                </div>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
 
-                <button type="submit" class="btn btn-primary btn-block"> Ubah </button>
+                </ul>
+            </div>
+        @endif
 
-            </form>
+        <div class="card shadow">
+            <div class="card-body">
+                <form action="{{ route('gallery.update', $item->id) }}" method="POST" enctype="multipart/form-data">
+
+                    @method('PUT')
+                    @csrf
+
+                    <div class="form-group">
+                        <label for="title">Gallery</label>
+                        <select name="travel_package_id" required class="form-control">
+                            <option value="{{ $item->travel_packages_id }}">Jangan Diubah</option>
+
+                            @foreach ($travel_packages as $travel_package)
+                                <option value="{{ $travel_package->id }}">
+                                    {{ $travel_package->title }}
+                                </option>
+                            @endforeach
+
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="image">IMAGE</label>
+                        <input type="file" class="form-control" name="image" placeholder="Image">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary btn-block"> Ubah </button>
+
+                </form>
+            </div>
         </div>
-    </div>
 
-</div>
-<!-- /.container-fluid -->
-    
+    </div>
+    <!-- /.container-fluid -->
+
 @endsection
-
--- resources/views/pages/success.blade.php
+++ resources/views/pages/success.blade.php
@@ -2,25 +2,26 @@
 @section('tittle', 'Checkout Success')
 
 @section('content')
-    
-    
-<main>
 
-    <div class="section-success d-flex align-items-center">
 
-        <div class="col text-center">
-            <img src="{{ url('nomads-template/frontend/images/ic_mail.png') }}" alt="">
+    <main>
 
-            <h1>Yeay! SucCESS</h1>
+        <div class="section-success d-flex align-items-center">
 
-            <P>We’ve sent you email for trip instruction
-                <br> 
-                please read it as well</P>
+            <div class="col text-center">
+                <img src="{{ url('nomads-template/frontend/images/ic_mail.png') }}" alt="">
 
-                <a href="{{ route('home') }}" class="btn btn-home-page mt-3 px-5"> Home Page</a>
+                <h1>Yeay! SucCESS</h1>
+
+                <P>We’ve sent you email for trip instruction
+                    <br>
+                    please read it as well
+                </P>
+
+                <a href="{{ route('/') }}" class="btn btn-home-page mt-3 px-5"> Home Page</a>
+            </div>
         </div>
-    </div>
 
-  </main>
+    </main>
 
 @endsection
